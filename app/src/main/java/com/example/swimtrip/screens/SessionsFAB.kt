82f69d241a3a5548bc57bd82c4bc115cca9c7051
AdhamCl <AdhamCl@outlook.com>
2024-07-07 12:15:14 +0100

    sconde commit

---
+++ app/src/main/java/com/example/swimtrip/screens/SessionsFAB.kt
@@ -0,0 +1,38 @@
+package com.example.swimtrip.screens
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SessionsFAB(addNewMember: () -> Unit) {
+
+    FloatingActionButton(
+        onClick = {
+            addNewMember()
+        },
+        containerColor = Color.Blue,
+
+
+        ) {
+        Row(
+            modifier =  Modifier.padding(10.dp),
+
+            ){
+
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "Add Button",
+                tint = Color.White,
+            )
+        }
+    }
+
+}
