b4e85337c4b2e57b2001ec102a5aec217992d78f
AdhamCl <AdhamCl@outlook.com>
2024-07-07 12:47:36 +0100

    sconde commit

---
+++ app/src/main/java/com/example/swimtrip/screens/archives/ArchivesScreen.kt
@@ -0,0 +1,10 @@
+package com.example.swimtrip.screens.archives
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ArchivesScreen() {
+    Text("ArchivesScreen")
+}
