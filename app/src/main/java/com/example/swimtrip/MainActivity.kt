bf3f305393cad2bf7aef04d2de74e4745adcb50a
AdhamCl <AdhamCl@outlook.com>
2024-07-07 12:11:34 +0100

    sconde commit

--- app/src/main/java/com/example/swimtrip/MainActivity.kt
+++ app/src/main/java/com/example/swimtrip/MainActivity.kt
@@ -1,33 +1,37 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.example.swimtrip
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
+import com.example.swimtrip.screens.HomeScreen
 import com.example.swimtrip.ui.theme.SwimTripTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val swimViewModel: SwimViewModel by viewModels()
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SwimTripTheme {
                 SetStatusBarColor(Color.Blue)
                 CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-                    Text("ddddd")
+                    HomeScreen(swimViewModel)
                 }
             }
         }
