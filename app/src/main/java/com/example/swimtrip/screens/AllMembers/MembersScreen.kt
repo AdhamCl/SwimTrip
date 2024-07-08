cbc4021616d2aa85cb41c8c4cc57739429f72798
AdhamCl <AdhamCl@outlook.com>
2024-07-08 17:20:46 +0100

    sconde commit

--- app/src/main/java/com/example/swimtrip/screens/AllMembers/MembersScreen.kt
+++ app/src/main/java/com/example/swimtrip/screens/AllMembers/MembersScreen.kt
@@ -1,15 +1,37 @@
 package com.example.swimtrip.screens.AllMembers
 
+import android.adservices.adselection.AddAdSelectionOverrideRequest
 import android.util.Log
+import android.widget.Toast
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -17,11 +39,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.swimmers.data.Members
 import com.example.swimtrip.SwimViewModel
 
 
+@ExperimentalMaterial3Api
 @Composable
 fun MembersScreen(
     swimViewModel: SwimViewModel
@@ -38,7 +63,7 @@ fun MembersScreen(
     }
 
     LazyColumn(
-        modifier = Modifier.padding(5.dp),
+        modifier = Modifier.fillMaxSize().padding(top = 5.dp, start = 2.dp, end = 2.dp, bottom = 5.dp),
         verticalArrangement = Arrangement.spacedBy(5.dp),
 
         ) {
@@ -48,29 +73,76 @@ fun MembersScreen(
         ) {
 
 
-            MemberItem(it)
+            val dismissState = rememberDismissState()
+            val dismissDirection = dismissState.dismissDirection
+
+            val isDismissed = dismissState.isDismissed(DismissDirection.StartToEnd) ||
+                    dismissState.isDismissed(DismissDirection.EndToStart)
+
+            val degrees: Float by animateFloatAsState(
+                if (dismissState.targetValue == DismissValue.Default) 0f else -45f,
+                label = ""
+            )
+
+            if (isDismissed) {
+                when (dismissDirection) {
+                    DismissDirection.EndToStart -> {
+                        swimViewModel.deleteMember(it.id)
+                    }
+                    DismissDirection.StartToEnd -> {
+                    }
+                    else -> {}
+                }
+            }
+
+
+            SwipeToDismiss(
+                state = dismissState,
+                background = { if(dismissDirection==DismissDirection.EndToStart) DeleteBackground(degrees)  else AddBackground(degrees) },
+                directions = setOf(DismissDirection.EndToStart,DismissDirection.StartToEnd),
+                dismissContent = {
+                    MemberItem(it)
+
+                }
+            )
+
         }
     }
 
 }
 
 @Composable
-fun MemberItem(member: Members) {
+fun MemberItem(
+    member: Members,
+
+) {
     Card(
         modifier = Modifier
-            .padding(8.dp)
+            .padding(top=5.dp, end = 5.dp, start = 5.dp)
             .fillMaxWidth(),
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Column(
+        Row(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            Text(text = "ID: ${member.id}")
-            Text(text = "Number: ${member.number}")
-            Text(text = "Name: ${member.firstName} ${member.lastName}")
-            Text(text = "Warnings: ${member.warning}")
-            Text(text = "Paid: ${if (member.isPay) "Yes" else "No"}")
+            Text(
+                modifier = Modifier.weight(1f),
+                text = member.number.toString()
+            )
+            Text(
+                modifier = Modifier.weight(3f),
+                text = member.firstName
+            )
+            Text(
+                modifier = Modifier.weight(3f),
+                text = member.lastName
+            )
+            Text(
+                modifier = Modifier.weight(0.5f),
+                text = member.warning.toString()
+            )
         }
     }
 }
