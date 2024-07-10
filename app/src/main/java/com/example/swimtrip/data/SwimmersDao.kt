2abc182cca9068f354ca388ce7660ef83b022416
AdhamCl <AdhamCl@outlook.com>
2024-07-10 13:04:16 +0100

    final commit

--- app/src/main/java/com/example/swimtrip/data/SwimmersDao.kt
+++ app/src/main/java/com/example/swimtrip/data/SwimmersDao.kt
@@ -9,20 +9,31 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface SwimmersDao {
 
-    @Query("SELECT * FROM members_table ORDER BY id ASC")
-    fun getAllMembers(): Flow<List<Members>>
+    @Query("SELECT * FROM members_table ORDER BY number ASC")
+    fun getAllMembers(): Flow<List<Member>>
 
 
-    @Query("SELECT * FROM members_table WHERE isChosen = 1 ORDER BY id ASC")
-    fun getChosenMembers(): Flow<List<Members>>
+    @Query("SELECT * FROM members_table WHERE isChosen = 1 ORDER BY number ASC")
+    fun getAllChosenMembers(): Flow<List<Member>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addNewMember(members: Members)
+    suspend fun addNewMember(members: Member)
 
+    @Query("SELECT EXISTS(SELECT 1 FROM members_table WHERE number = :number LIMIT 1)")
+    suspend fun isMemberExists(number: Int): Boolean
+
+    @Query("SELECT EXISTS(SELECT 1 FROM members_table WHERE id = :id AND isChosen = 1 LIMIT 1)")
+    suspend fun isChosen(id: Int): Boolean
     @Update
-    suspend fun updateMember(members: Members)
+    suspend fun updateMember(members: Member)
 
     @Query("DELETE FROM members_table WHERE id = :memberId")
     suspend fun deleteMemberById(memberId: Int)
+
+    @Query("SELECT COUNT(*) FROM members_table WHERE isChosen = 1")
+    suspend fun getChosenMembersCount(): Int
+
+    @Query("SELECT COUNT(*) FROM members_table WHERE isChosen = 1 AND isPay = 1")
+    suspend fun getChosenAndPaidMembersCount(): Int
 
 }
