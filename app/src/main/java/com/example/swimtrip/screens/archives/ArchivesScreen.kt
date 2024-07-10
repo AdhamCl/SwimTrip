e2df80f465a3f58cdd92430f432fd87ac40044e5
AdhamCl <AdhamCl@outlook.com>
2024-07-10 17:56:07 +0100

    final commit

--- app/src/main/java/com/example/swimtrip/screens/archives/ArchivesScreen.kt
+++ app/src/main/java/com/example/swimtrip/screens/archives/ArchivesScreen.kt
@@ -1,10 +1,35 @@
 package com.example.swimtrip.screens.archives
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.swimmers.data.Member
+import com.example.swimtrip.SwimViewModel
+import com.example.swimtrip.screens.AllMembers.DeleteBackground
+import com.example.swimtrip.screens.AllMembers.MemberItem
 
 
+@ExperimentalMaterial3Api
 @Composable
-fun ArchivesScreen() {
-    Text("ArchivesScreen")
+fun ArchivesScreen(
+    swimViewModel: SwimViewModel
+
+) {
+
+
 }
