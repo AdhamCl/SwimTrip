dfc1242be935d752d30df24503e8e9df6353e3bf
AdhamCl <AdhamCl@outlook.com>
2024-07-10 17:56:48 +0100

    final commit

---
+++ app/src/main/java/com/example/swimtrip/screens/chosen/ChosenTopBar.kt
@@ -0,0 +1,51 @@
+package com.example.swimtrip.screens.chosen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.swimmers.data.Member
+
+@Composable
+fun ChosenTopBar(
+    chosenAndPaidMembersCount: Int,
+    chosenMembersCount: Int,
+) {
+
+
+
+
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            modifier = Modifier.weight(5f),
+            text = "المختارون: ${chosenMembersCount} ",
+            fontSize = 22.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            modifier = Modifier.weight(5f),
+            text = "الدافغون: ${chosenAndPaidMembersCount}",
+            fontSize = 22.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+
+
+
+    }
+}
