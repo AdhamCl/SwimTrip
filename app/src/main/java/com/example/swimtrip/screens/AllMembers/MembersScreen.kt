d0da44e4b698f9431d7cc1ad9152d7add1ae0791
AdhamCl <AdhamCl@outlook.com>
2024-07-12 21:26:39 +0100

    final commit

--- app/src/main/java/com/example/swimtrip/screens/AllMembers/MembersScreen.kt
+++ app/src/main/java/com/example/swimtrip/screens/AllMembers/MembersScreen.kt
@@ -1,12 +1,12 @@
 package com.example.swimtrip.screens.AllMembers
 
 
+import android.widget.Toast
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -31,12 +31,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.example.swimmers.data.Members
+import androidx.compose.ui.unit.sp
+import com.example.swimmers.data.Member
 import com.example.swimtrip.SwimViewModel
+import com.example.swimtrip.ui.theme.MayaBlue
+import kotlinx.coroutines.launch
 
 
 @ExperimentalMaterial3Api
@@ -49,7 +55,8 @@ fun MembersScreen(
 
 
 
-
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
 
     LaunchedEffect(key1 = true) {
         swimViewModel.getAllMembers()
@@ -86,11 +93,26 @@ fun MembersScreen(
                 background = { DeleteBackground(degrees) },
                 directions = setOf(DismissDirection.EndToStart),
                 dismissContent = {
-                    MemberItem(it){
-                        swimViewModel.isChosen.value = true
-                        swimViewModel.updateMember()
-                    }
+                    MemberItem(it) {
+                        scope.launch {
 
+                            if (!swimViewModel.checkMemberIsChosen(it.id)) {
+                                swimViewModel.isChosen.value = true
+                                val member = Member(
+                                    id = it.id,
+                                    number = it.number,
+                                    firstName = it.firstName,
+                                    lastName = it.lastName,
+                                    warning = it.warning,
+                                    isChosen = true,
+                                    isPay = false,
+                                )
+                                swimViewModel.updateMember(member)
+                            } else
+                                Toast.makeText(context, "", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
                 }
             )
 
@@ -101,7 +123,7 @@ fun MembersScreen(
 
 @Composable
 fun MemberItem(
-    member: Members,
+    member: Member,
     onAddClicked:()->Unit
     ) {
     Card(
@@ -120,34 +142,35 @@ fun MemberItem(
             Spacer(modifier = Modifier.width(10.dp))
             Text(
                 modifier = Modifier.weight(1f),
-                text = member.number.toString()
+                text = member.number.toString(),
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
             )
-            Column(
-                modifier = Modifier.weight(3f),
-                verticalArrangement = Arrangement.SpaceAround,
 
-                ) {
                 Text(
-                    text = member.firstName
+                    modifier = Modifier.weight(4f),
+
+                    text = member.firstName+" "+member.lastName,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
                 )
-                Spacer(modifier = Modifier.height(10.dp))
-                Text(
-                    text = member.lastName
-                )
-            }
+
+
 
             Text(
                 modifier = Modifier.weight(.5f),
-                text = member.warning.toString()
+                text = member.warning.toString(),
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
             )
             IconButton(
-                onClick = { },
+                onClick = { onAddClicked() },
                 modifier = Modifier.weight(.5f)
             ) {
                 Icon(
                     Icons.Default.Add,
                     contentDescription = "Add to Other",
-                    tint = Color.Blue,
+                    tint = MayaBlue,
                     modifier = Modifier.size(32.dp)
                 )
             }
