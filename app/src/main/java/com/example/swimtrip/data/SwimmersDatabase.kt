651b6046124081299b1338b2776b02dcdac41128
AdhamCl <AdhamCl@outlook.com>
2024-08-01 20:01:51 +0100

    final commit

--- app/src/main/java/com/example/swimtrip/data/SwimmersDatabase.kt
+++ app/src/main/java/com/example/swimtrip/data/SwimmersDatabase.kt
@@ -2,15 +2,18 @@ package com.example.swimmers.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-
+import androidx.room.TypeConverters
+import com.example.swimtrip.data.Archive
+import com.example.swimtrip.data.Converters
 
 
 @Database(
     entities = [
         Member::class,
-
+        Archive::class
     ], version = 1, exportSchema = false
 )
+@TypeConverters(Converters::class)
 
 abstract class SwimmersDatabase : RoomDatabase() {
     abstract fun swimmersDao(): SwimmersDao
