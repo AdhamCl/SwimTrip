fbdf48b3821fc62fadfc2fa2c0e1c772722e17bb
AdhamCl <AdhamCl@outlook.com>
2024-08-01 19:59:55 +0100

    final commit

--- app/src/main/java/com/example/swimtrip/Constants.kt
+++ app/src/main/java/com/example/swimtrip/Constants.kt
@@ -3,7 +3,8 @@ package com.example.swimtrip
 object Constants {
 
     const val DATABASE_MEMBERS_TABLE = "members_table"
+    const val DATABASE_ARCHIVE_TABLE = "archive_table"
 
-    const val SWIM_TRIP_DAtABASE = "members_databaseeeee"
+    const val SWIM_TRIP_DAtABASE = "members_databasdeeeee"
 
 }
