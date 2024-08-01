8350c362feb0289a42c091c51699eab29395e69c
AdhamCl <AdhamCl@outlook.com>
2024-08-01 20:00:52 +0100

    final commit

--- app/src/main/java/com/example/swimtrip/screens/DeleteDialog.kt
+++ app/src/main/java/com/example/swimtrip/screens/DeleteDialog.kt
@@ -5,10 +5,9 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,25 +17,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.swimtrip.data.Archive
-import com.example.swimtrip.screens.archives.MemberItem
 import kotlinx.coroutines.delay
 
 
 
 @Composable
 fun DeleteDialog(
-    onDismissRequest: () -> Unit
+    text: String,
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
 ) {
-
 
     var startAnimation by remember { mutableStateOf(false) }
 
     val alphaState by animateFloatAsState(
-        targetValue = if (startAnimation) 1f else 0f,
+        targetValue = if(startAnimation) 1f else 0f,
         animationSpec = tween(100), label = ""
     )
 
@@ -44,8 +44,8 @@ fun DeleteDialog(
 
     Box(
 
-    ) {
-        LaunchedEffect(key1 = Unit) {
+    ){
+        LaunchedEffect(key1 = Unit){
             delay(100)
             startAnimation = true
         }
@@ -58,38 +58,45 @@ fun DeleteDialog(
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp),
+                        .padding(bottom = 8.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        text = "قائمة المعنيون",
+                        text = "تنبيه!",
+                        fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
-                        fontSize = 32.sp
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
                     )
                 }
             },
             text = {
-                if (archive?.members.isNullOrEmpty()) {
-                    Text(text = "قائمة فارغة.")
-                } else {
-                    LazyColumn(
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-
-                        items(
-                            items = archive!!.members,
-                            key = { it.id }
-                        ) { member ->
-                            MemberItem(member)
-                        }
-                    }
-                }
+                Text(
+                    text = text,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Gray
+                )
             },
             confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation()
+                        onDismissRequest()
 
+                    }
+                ) {
+                    Text("تأكيد")
+                }
             },
             dismissButton = {
-
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text("إلغاء")
+                }
             }
         )
     }
