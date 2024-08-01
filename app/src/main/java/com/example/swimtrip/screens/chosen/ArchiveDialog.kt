803448e4fdd1e0b99f4841a9b614912c707787d9
AdhamCl <AdhamCl@outlook.com>
2024-08-01 19:53:33 +0100

    final commit

--- app/src/main/java/com/example/swimtrip/screens/chosen/ArchiveDialog.kt
+++ app/src/main/java/com/example/swimtrip/screens/chosen/ArchiveDialog.kt
@@ -1,9 +1,13 @@
 package com.example.swimtrip.screens.chosen
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -13,9 +17,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 
 
@@ -23,6 +33,8 @@ import kotlinx.coroutines.delay
 fun ArchiveDialog(
     onDismissRequest: () -> Unit,
     onConfirmation: () -> Unit,
+    listsSize: Int,
+    context: Context
 ) {
 
     var startAnimation by remember { mutableStateOf(false) }
@@ -42,23 +54,42 @@ fun ArchiveDialog(
             startAnimation = true
         }
         AlertDialog(
-            modifier = Modifier
-                .alpha(alphaState),
-            title = {
-                Text(  "")
-            },
-            text = {
-                Text(  "هل أنت متأد انك اتريد ارشفة هاته الرحلة.")
-
-
-            },
+            modifier = Modifier.alpha(alphaState),
             onDismissRequest = {
                 onDismissRequest()
+            },
+            title = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "تنبيه!",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            },
+            text = {
+                Text(
+                    text = "هل أنت متأكد أنك تريد أرشفة هذه الرحلة.",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Gray
+                )
             },
             confirmButton = {
                 TextButton(
                     onClick = {
-                        onConfirmation()
+                        if(listsSize==0) Toast.makeText(context,"القائمة فارغة",Toast.LENGTH_SHORT).show()
+                        else  onConfirmation()
+
+                        onDismissRequest()
+
                     }
                 ) {
                     Text("تأكيد")
@@ -70,7 +101,7 @@ fun ArchiveDialog(
                         onDismissRequest()
                     }
                 ) {
-                    Text("الغاء")
+                    Text("إلغاء")
                 }
             }
         )
