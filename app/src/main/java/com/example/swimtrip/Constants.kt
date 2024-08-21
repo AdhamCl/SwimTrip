c57b533d54213c38ea705c336c773981e07ab9d8
Adham <Adhamcl@outlook.com>
2024-08-21 18:43:59 +0100

    final commit
    Signed-off-by: Adham <Adhamcl@outlook.com>

--- app/src/main/java/com/example/swimtrip/Constants.kt
+++ app/src/main/java/com/example/swimtrip/Constants.kt
@@ -5,6 +5,6 @@ object Constants {
     const val DATABASE_MEMBERS_TABLE = "members_table"
     const val DATABASE_ARCHIVE_TABLE = "archive_table"
 
-    const val SWIM_TRIP_DAtABASE = "members_databasdeeeee"
+    const val SWIM_TRIP_DAtABASE = "members_databasdeedeee"
 
 }
