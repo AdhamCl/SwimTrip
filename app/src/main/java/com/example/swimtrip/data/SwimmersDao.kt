40cf32c10d6306a88d2c9f984d8f94c7c21ef8e1
Adham <Adhamcl@outlook.com>
2024-08-21 18:45:14 +0100

    final commit
    Signed-off-by: Adham <Adhamcl@outlook.com>

--- app/src/main/java/com/example/swimtrip/data/SwimmersDao.kt
+++ app/src/main/java/com/example/swimtrip/data/SwimmersDao.kt
@@ -13,15 +13,39 @@ interface SwimmersDao {
     @Query("SELECT * FROM members_table ORDER BY number ASC")
     fun getAllMembers(): Flow<List<Member>>
 
+    @Query("SELECT * FROM members_table WHERE level = 'متوسط' ORDER BY number ASC")
+    fun getMembersByIntermediateLevel(): Flow<List<Member>>
 
-    @Query("SELECT * FROM members_table WHERE isChosen = 1 ORDER BY number ASC")
+    @Query("SELECT * FROM members_table WHERE level = 'ابتدائي' ORDER BY number ASC")
+    fun getMembersByPrimaryLevel(): Flow<List<Member>>
+
+
+    @Query("""
+        SELECT * FROM members_table 
+        WHERE isChosen = 1 
+        ORDER BY 
+            CASE 
+                WHEN level = 'متوسط' THEN 0 
+                WHEN level = 'ابتدائي' THEN 1 
+                ELSE 2 
+            END, 
+            number ASC
+    """)
     fun getAllChosenMembers(): Flow<List<Member>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addNewMember(members: Member)
 
-    @Query("SELECT EXISTS(SELECT 1 FROM members_table WHERE number = :number LIMIT 1)")
-    suspend fun isMemberExists(number: Int): Boolean
+    @Query("""
+        SELECT EXISTS(
+            SELECT 1 
+            FROM members_table 
+            WHERE number = :number 
+              AND level = :level 
+            LIMIT 1
+        )
+    """)
+    suspend fun isMemberExists(number: Int, level: String): Boolean
 
     @Query("SELECT EXISTS(SELECT 1 FROM members_table WHERE id = :id AND isChosen = 1 LIMIT 1)")
     suspend fun isChosen(id: Int): Boolean
